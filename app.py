33867ded7a55e2b116cc5905e3b6327aee575269
kotatharun <kotatharunreddy@gmail.com>
2026-05-03 14:51:38 -0500

    CSS Added to the pages

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.guests import guests_bp
 from routes.reservations import reservations_bp
 from routes.dashboard import dashboard_bp
 
 app = Flask(__name__)
-app.secret_key = "hotel_secret_key"
 
+# Needed for flash messages
+app.secret_key = "hotel_management_secret_key"
+
+# Register route files
 app.register_blueprint(guests_bp)
 app.register_blueprint(reservations_bp)
 app.register_blueprint(dashboard_bp)
 
+
 @app.route("/")
 def home():
-    return """
-    <h1>Hotel Management System</h1>
-    <a href='/guests'>Guests CRUD</a><br>
-    <a href='/reservations'>Reservations CRUD</a><br>
-    <a href='/dashboard'>Summary Dashboard</a>
-    """
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
